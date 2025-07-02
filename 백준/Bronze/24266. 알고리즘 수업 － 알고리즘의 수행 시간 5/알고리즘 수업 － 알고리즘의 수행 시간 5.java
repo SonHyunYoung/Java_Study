102ec61e94079ae9b74d58ab7b653052c80c51a0
SonHyunYoung <son2497474@gmail.com>
2025-07-02 11:33:43 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 108 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+   public class Main  {
+      
+      public static void main(String [] args) throws IOException {
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+         
+         long n = Long.parseLong(br.readLine());
+         long count = n * n * n;
+
+         bw.write(String.valueOf(count));
+         bw.newLine();
+         bw.write(String.valueOf(3));
+         bw.flush();
+         
+         bw.close();
+         br.close();
+         
+        }
+   }	
